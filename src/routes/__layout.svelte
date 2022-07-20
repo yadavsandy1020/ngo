b471ef32f7459deb8514e7ed70365badf1a56c79
Sandeep Yadav <sandeep.yadav.consultant@heart.org>
2022-07-20 14:05:59 +0530

    social media icons added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,4 @@
-<nav
-  class="navbar navbar-expand-lg navbar-light"
-  style="background-color: white;"
->
+<nav class="navbar navbar-expand-lg navbar-light bg-custom-2 ">
   <div class="container-fluid">
     <button
       class="navbar-toggler"
@@ -18,9 +15,34 @@
       <img src="/ngo-logo.png" alt="Logo" />
     </a>
     <div class="d-flex flex-column justify-content-between align-items-end">
-      <div class="pb-5">Hello</div>
+      <div class="pb-3 d-flex justify-content-between">
+        <span style="color: #00acee;">
+          <a
+            href="https://www.facebook.com/Royalkrishnagroup24/"
+            target="_blank"><i class="fa-brands fa-twitter fa-beat" /></a
+          >
+        </span>
+        <span style="color: #4267B2;">
+          <i class="fa-brands fa-facebook fa-beat" />
+        </span>
+        <span style="color: #8a3ab9;">
+          <a
+            href="https://www.instagram.com/royalkrishnagroup24/"
+            target="_blank"><i class="fa-brands fa-instagram fa-beat" /></a
+          >
+        </span>
+        <span style="color: #FF0000;">
+          <a
+            href="https://www.youtube.com/channel/UCaan_z4_PPLBxVGNRzeVVng"
+            target="_blank"><i class="fab fa-youtube fa-beat" /></a
+          >
+        </span>
+      </div>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="/members">Members</a>
           </li>
@@ -37,15 +59,6 @@
             <a class="nav-link" href="/contactus">Contact us</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </div>
@@ -56,5 +69,15 @@
 <style>
   nav {
     margin-bottom: 20px;
+  }
+  i {
+    padding: 10px;
+    cursor: pointer;
+  }
+  a {
+    color: unset;
+  }
+  .bg-custom-2 {
+    background-image: white;
   }
 </style>
